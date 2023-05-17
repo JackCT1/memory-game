3879b508d6fe2c5692bd7cb7d02d410d11d5f0ba
JackCT1 <jackthomas933@gmail.com>
2023-05-17 12:39:04 +0100

    image array

--- app.js
+++ app.js
@@ -1,1 +1,54 @@
-const cardArray = [{}];
+const cardArray = [
+  {
+    name: "cheeseburger",
+    image: "images/cheeseburger.png",
+  },
+  {
+    name: "fries",
+    image: "images/fries.png",
+  },
+  {
+    name: "hotdog",
+    image: "images/hotdog.png",
+  },
+  {
+    name: "ice-cream",
+    image: "images/ice-cream.png",
+  },
+  {
+    name: "milkshake",
+    image: "images/milkshake.png",
+  },
+  {
+    name: "pizza",
+    image: "images/pizza.png",
+  },
+  {
+    name: "cheeseburger",
+    image: "images/cheeseburger.png",
+  },
+  {
+    name: "fries",
+    image: "images/fries.png",
+  },
+  {
+    name: "hotdog",
+    image: "images/hotdog.png",
+  },
+  {
+    name: "ice-cream",
+    image: "images/ice-cream.png",
+  },
+  {
+    name: "milkshake",
+    image: "images/milkshake.png",
+  },
+  {
+    name: "pizza",
+    image: "images/pizza.png",
+  },
+];
+
+cardArray.sort(() => 0.5 - Math.random());
+
+const gridDisplay = document.querySelector("#grid");
